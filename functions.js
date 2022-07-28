9da3c5d71fc24c62cdd4cc9fd04a21c3f9356151
originallykevin <ly1@ualberta.ca>
2022-07-28 04:30:46 +0000

    created a function with return. printed the function

---
+++ functions.js
@@ -0,0 +1,10 @@
+const notBananas = 'bananas';
+
+function eat(food) {
+  return food + ' tasted really good.'
+  
+}
+
+// const eat = (food) => food + ' tasted really good.'
+
+console.log(eat(notBananas))
